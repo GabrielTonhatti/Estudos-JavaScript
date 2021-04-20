7c5623c21452b5de9c2e571cde6113accc510007
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2021-04-20 18:07:05 -0300

    update closure

--- Curso JavaScript/Função/closure.js
+++ Curso JavaScript/Função/closure.js
@@ -1,4 +1,4 @@
-// Closure é o escoó criado quando uma função é declarada
+// Closure é o escopo criado quando uma função é declarada
 // Esse escopo permite a função acessar e manipular variáveis externas à função
 
 // Contexto léxico em ação!
