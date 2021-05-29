9fea2ed19629720bee04000cbd8a7d40f50edfb4
GabrielTonhatti <gabrieltonhatti37@gmail.com>
2021-05-28 23:35:31 -0300

    Fundamentos JavaScript Funcional

---
+++ Fundamentos JavaScript Funcional/foundation/before.js
@@ -0,0 +1,17 @@
+//1. Prerequisites
+
+// -> Basic knowledge of JS
+// ---> Data types: number, string, boolean, array, and object
+// ---> Operator(+, -, *, /, &&, ||, >=, <=,...)
+// ---> If, For, While
+
+// 2. Node
+// -> NodeJS (v14.6.0)
+
+// 3. Visual Studio Code
+// -> Code Runner
+// --> Coderunner: clear Previous Output
+// --> Crlt + Alt + n (run code)
+// --> Crlt + Alt + m (stop the running code)
+
+console.log('test!')
---
+++ Fundamentos JavaScript Funcional/funcional/first-class-function.js
@@ -0,0 +1,7 @@
+/*
+ * A programming language is said to have
+ * First-class function when function in
+ *that language are treated like any other
+ * variable
+ */
+
